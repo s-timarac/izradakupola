85325c173cea99083fd645d306db61343c83923d
Sofija <sofdev.c@gmail.com>
2026-03-28 21:30:02 +0100

    Initial commit

---
+++ ss3.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+        fontFamily: {
+            sans: ['Source Sans Pro', 'sans-serif'],
+        },
+        colors: {
+            brand: {
+            yellow: '#ffc646',
+            blue: '#177eb5',
+            navy: '#336699',
+            dark: '#070707',
+            gray: '#494949',
+            lightbg: '#f1f4f9',
+            mediumbg: '#e2e9f3',
+            }
+        }
+        }
+    }
+}
